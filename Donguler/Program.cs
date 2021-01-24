890c330e5cb46422cbab528200d58b7c8dc83d6d
Özkan Yılmaz <ozkanyilmaz12@icloud.com>
2021-01-24 13:30:55 +0300

    20 Ocak 2021 Ders Sonu

---
+++ Donguler/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım geliştirici yetiştirme kampı";
+            string kurs2 = "Programlamaya başlangıç kursu";
+            string kurs3 = "Java";
+
+            //array - dizi
+
+            string[] kurslar = new string[] { "Yazılım geliştirici yetiştirme kampı", "Programlamaya başlangıç kursu", "Java", "Python", "C#" };
+
+            for (int i = 0; i < kurslar.Length; i++)
+            {
+                Console.WriteLine(kurslar[i]);
+            }
+
+            Console.WriteLine("For bitti");
+            foreach (string kurs in kurslar)
+            {
+                Console.WriteLine(kurs);
+            }
+
+            Console.WriteLine("Sayfa sonu - footer");
+        }
+    }
+}
